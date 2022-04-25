984d5583b1a7dfd659e52d7619574335f3562aa9
olasunkanmi <olasunkanmizik@gmail.com>
2022-04-25 17:13:58 +0100

    project

---
+++ src/main/java/task/lendapp/LendAppApplication.java
@@ -0,0 +1,13 @@
+package task.lendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task/lendapp/LendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package task.lendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
